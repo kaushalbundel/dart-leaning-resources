9daa4d02a78b48d22d44a45e82c0eea7235c3a4b
Kaushal Bundel <kaushalbundel@gmail.com>
2026-05-26 07:23:43 +0530

    command runner created

--- dartpedia/command_runner/lib/command_runner.dart
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -1,6 +1,5 @@
-library;
-// any common code in dart can also be termed as library
-// difference between a library and a package is that packages are shareable whereas libraries are reusable code blocks that may or may not be shareable
-
-export 'src/command_runner_base.dart';
-
+class CommandRunner {
+    Future<void> run(List<String> input) async {
+        print('CommandRunner arguments: ${input}');
+    }
+}
